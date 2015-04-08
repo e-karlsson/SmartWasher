e1f35cb186b146f10a7578fd240de9edbc37084a
mr3k <emil_k15@hotmail.com>
2015-04-08 18:29:49 +0200

    Added test comment

--- app/src/main/java/com/washer/smart/smartwasher/MainActivity.java
+++ app/src/main/java/com/washer/smart/smartwasher/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //Tests
+    //Test2
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
